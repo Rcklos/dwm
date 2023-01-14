ee9622e668b2fd09ee26adfcd4e37332cb73950b
rcklos <2362819691@qq.com>
2023-01-15 00:37:26 +0800

    key: flameshot

--- config.def.h
+++ config.def.h
@@ -65,8 +65,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+// flameshot
+static const char *flameshotcmd[] = { "flameshot", "gui", NULL };
+static const char *flameshotscreencmd[] = { "flameshot", "screen", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = flameshotcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotscreencmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
