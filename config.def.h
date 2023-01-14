b328e7cf3ad7ea940af0120a14012cf285e517ed
rcklos <2362819691@qq.com>
2023-01-15 00:42:29 +0800

    custom: font and color

--- config.def.h
+++ config.def.h
@@ -10,21 +10,30 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {
+    "JetBrainsMono Nerd Font Mono:size=14:antialias=true:autohint=true",
+    "RobotoMono Nerd Font:size=14:antialias=true:autohint=true",
+    "Hack Nerd Font:size=14:antialias=true:autohint=true",
+    "BlexMono Nerd Font Mono:size=14:antialias=true:autohint=true",
+    "SauceCodePro Nerd Font Mono:size=14:antialias=true:autohint=true",
+    "JoyPixels:size=12:antialias=true:autohint=true" };
+static const char dmenufont[]      = "SauceCodePro Nerd Font Mono:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#37474f";
+static const char col_border[]      = "#42A5F5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
 	/* xprop(1):
